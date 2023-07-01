19636b3b1febf9d9ead8f57849ac0ed9d1406168
Heinan Cabouly <heinancabouly@gmail.com>
2023-07-01 16:49:48 +0300

    End of module 6

--- instances.tf
+++ instances.tf
@@ -8,6 +8,7 @@ resource "aws_instance" "web_server_one" {
   ami                         = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
   instance_type               = var.instance_sizes["micro"]
   vpc_security_group_ids      = [aws_security_group.nginx.id]
+  iam_instance_profile        = aws_iam_instance_profile.nginx_profile.name
   subnet_id                   = aws_subnet.public_subnet_one.id
   associate_public_ip_address = true
   tags = {
@@ -17,12 +18,18 @@ resource "aws_instance" "web_server_one" {
     BillingCode = local.common_tags.BillingCode
   }
 
+  depends_on = [aws_iam_role_policy.allow_s3_all]
+
   user_data = <<-EOF
         #!/bin/bash
+        echo "Hello from AWS!"
         sudo amazon-linux-extras install -y nginx1
         sudo service nginx start
+        aws s3 cp s3://${aws_s3_bucket.s3.id}/index.html /home/ec2-user/index.html
+        aws s3 cp s3://${aws_s3_bucket.s3.id}/Globo_logo_Vert.png /home/ec2-user/Globo_logo_Vert.png
         sudo rm /usr/share/nginx/html/index.html
-        echo '<html><head><title>Taco Team Server One</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
+        sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html
+        sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png
     EOF  
 }
 
@@ -31,6 +38,7 @@ resource "aws_instance" "web_server_two" {
   ami                         = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
   instance_type               = var.instance_sizes["micro"]
   vpc_security_group_ids      = [aws_security_group.nginx.id]
+  iam_instance_profile        = aws_iam_instance_profile.nginx_profile.name
   subnet_id                   = aws_subnet.public_subnet_two.id
   associate_public_ip_address = true
   tags = {
@@ -40,81 +48,71 @@ resource "aws_instance" "web_server_two" {
     BillingCode = local.common_tags.BillingCode
   }
 
+  depends_on = [aws_iam_role_policy.allow_s3_all]
+
   user_data = <<-EOF
         #!/bin/bash
+        echo "Hello from AWS!"
         sudo amazon-linux-extras install -y nginx1
         sudo service nginx start
+        aws s3 cp s3://${aws_s3_bucket.s3.id}/index.html /home/ec2-user/index.html
+        aws s3 cp s3://${aws_s3_bucket.s3.id}/Globo_logo_Vert.png /home/ec2-user/Globo_logo_Vert.png
         sudo rm /usr/share/nginx/html/index.html
-        echo '<html><head><title>Taco Team Server Two</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
+        sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html
+        sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png
     EOF  
 }
 
 # aws_iam_role
-resource "aws_iam_role" "s3_role" {
-  name = "s3_website_role"
+resource "aws_iam_role" "allow_nginx_s3" {
+  name = "allow_nginx_s3"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "s3.amazonaws.com"
-        }
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
       },
-    ]
-  })
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = local.common_tags
 }
 
-# aws_iam_role_policy
-resource "aws_iam_role_policy" "s3_policy" {
-  name = "s3_website_policy"
-  role = aws_iam_role.s3_role.id
+resource "aws_iam_instance_profile" "nginx_profile" {
+  name = "nginx_profile"
+  role = aws_iam_role.allow_nginx_s3.name
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "AWS" : "${data.aws_elb_service_account.main.arn}"
-        },
-        "Action" : "s3:PutObject",
-        "Resource" : "arn:aws:s3:::${local.s3_bucket_name}/nginx-lb/*"
-      },
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "delivery.logs.amazonaws.com"
-        },
-        "Action" : "s3:PutObject",
-        "Resource" : "arn:aws:s3:::${local.s3_bucket_name}/nginx-lb/*",
-        "Condition" : {
-          "StringEquals" : {
-            "s3:x-amz-acl" : "bucket-owner-full-control"
-          }
-        }
-      },
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "delivery.logs.amazonaws.com"
-        },
-        "Action" : "s3:GetBucketAcl",
-        "Resource" : "arn:aws:s3:::${local.s3_bucket_name}"
-      }
-    ]
-  })
+  tags = local.common_tags
 }
 
-# aws_iam_instance_profile
-resource "aws_iam_instance_profile" "s3_profile" {
-  name = "s3_profile"
-  role = aws_iam_role.s3_role.name
+resource "aws_iam_role_policy" "allow_s3_all" {
+  name = "allow_s3_all"
+  role = aws_iam_role.allow_nginx_s3.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+                "arn:aws:s3:::${local.s3_bucket_name}",
+                "arn:aws:s3:::${local.s3_bucket_name}/*"
+            ]
+    }
+  ]
+}
+EOF
+
 }
--- s3.tf
+++ s3.tf
@@ -1,13 +1,46 @@
 # aws_s3_bucket
 resource "aws_s3_bucket" "s3" {
   bucket = local.s3_bucket_name
-
+  force_destroy = true
 }
 
 # aws_s3_bucket_policy
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.s3.id
-  policy = aws_iam_role_policy.s3_policy.policy
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "${data.aws_elb_service_account.main.arn}"
+        },
+        "Action" : "s3:PutObject",
+        "Resource" : "arn:aws:s3:::${local.s3_bucket_name}/nginx-lb/*"
+      },
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "delivery.logs.amazonaws.com"
+        },
+        "Action" : "s3:PutObject",
+        "Resource" : "arn:aws:s3:::${local.s3_bucket_name}/nginx-lb/*",
+        "Condition" : {
+          "StringEquals" : {
+            "s3:x-amz-acl" : "bucket-owner-full-control"
+          }
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "delivery.logs.amazonaws.com"
+        },
+        "Action" : "s3:GetBucketAcl",
+        "Resource" : "arn:aws:s3:::${local.s3_bucket_name}"
+      }
+    ]
+  })
 }
 
 # aws_s3_object
